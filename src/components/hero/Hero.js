55f762c5b8bb44f1c40b5fe0f40c85a7fca9a24a
Kiah-tech <120767657+Kiah-tech@users.noreply.github.com>
2024-07-28 03:24:13 -0400

    Update Hero.js

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -1,38 +1,53 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import { Button, NoRightPaddingContainer } from '../../styles/Common.style';
 import { H1, Para } from '../../styles/Typography.style';
 import {
-	HeroWrapper,
-	HeroContent,
-	HeroImage,
-	HeroContentWrapper,
+    HeroWrapper,
+    HeroContent,
+    HeroImage,
+    HeroContentWrapper,
+    ButtonWrapper
 } from './Hero.style';
 import HeroImg from '../../images/image-mockups.png';
 
 const Hero = () => {
-	return (
-		<div>
-			<NoRightPaddingContainer>
-				<HeroWrapper>
-					<HeroContent>
-						<HeroContentWrapper>
-							<H1> Quick Funds, Next Generation Digital Banking </H1>
-							<Para>
-							Take your financial life online. Your Easybank 
-							account will be a one-stop-shop for spending,
-							 saving, budgeting, investing, and much more.
-							</Para>
-							<Button>Login</Button>
-						</HeroContentWrapper>
-					</HeroContent>
-					<HeroImage>
-						{/* <img src={HeroImg} alt='hero' /> */}
-						{/* Replace img here */}
-					</HeroImage>
-				</HeroWrapper>
-			</NoRightPaddingContainer>
-		</div>
-	);
+    const navigate = useNavigate();
+
+    const handleLoginClick = () => {
+        navigate('/login');
+    };
+
+    const handleSignUpClick = () => {
+        navigate('/signup');
+    };
+
+    return (
+        <div>
+            <NoRightPaddingContainer>
+                <HeroWrapper>
+                    <HeroContent>
+                        <HeroContentWrapper>
+                            <H1> Quick Funds, Next Generation Digital Banking </H1>
+                            <Para>
+                                Take your financial life online. Your Easybank
+                                account will be a one-stop-shop for spending,
+                                saving, budgeting, investing, and much more.
+                            </Para>
+                            <ButtonWrapper>
+                                <Button onClick={handleLoginClick}>Login</Button>
+                                <Button onClick={handleSignUpClick}>Sign Up</Button>
+                            </ButtonWrapper>
+                        </HeroContentWrapper>
+                    </HeroContent>
+                    <HeroImage>
+                        {/* <img src={HeroImg} alt='hero' /> */}
+                        {/* Replace img here */}
+                    </HeroImage>
+                </HeroWrapper>
+            </NoRightPaddingContainer>
+        </div>
+    );
 };
 
 export default Hero;
