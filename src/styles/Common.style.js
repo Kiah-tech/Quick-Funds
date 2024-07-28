6ce9758f92c069729300d60f05d0ccc2cc6ed640
Kiah-tech <120767657+Kiah-tech@users.noreply.github.com>
2024-07-28 03:22:11 -0400

    Update Common.style.js

--- src/styles/Common.style.js
+++ src/styles/Common.style.js
@@ -1,33 +1,54 @@
 import styled from 'styled-components';
 
 export const Button = styled.button`
-	padding: 1rem 2rem;
-	border-radius: 3rem;
-	background: linear-gradient(
-		45deg,
-		var(--LimeGreen) 0%,        
-		var(--BrightCyan) 100%
-	);
-	border: none;
-	outline: none;
-	color: var(--White);
+    padding: 1rem 2rem;
+    border-radius: 3rem;
+    background: linear-gradient(
+        45deg,
+        var(--LimeGreen) 0%,
+        var(--BrightCyan) 100%
+    );
+    border: none;
+    outline: none;
+    color: var(--White);
+    cursor: pointer;
+    transition: background 0.3s, transform 0.1s;
+
+    &:hover {
+        background: linear-gradient(
+            45deg,
+            var(--BrightCyan) 0%,
+            var(--LimeGreen) 100%
+        );
+    }
+
+    &:active {
+        background: linear-gradient(
+            45deg,
+            var(--BrightCyan) 0%,
+            var(--LimeGreen) 100%
+        );
+        transform: scale(0.95);
+    }
 `;
+
 export const Container = styled.div`
-	padding: 0 10rem;
-	@media (max-width: 991px) {
-		padding: 0 6rem;
-	}
-	@media (max-width: 480px) {
-		padding: 0 2rem;
-	}
+    padding: 0 10rem;
+    @media (max-width: 991px) {
+        padding: 0 6rem;
+    }
+    @media (max-width: 480px) {
+        padding: 0 2rem;
+    }
 `;
 
 export const NoRightPaddingContainer = styled.div`
-	padding: 0 0 0 10rem;
-	@media (max-width: 991px) {
-		padding: 0 0 0 4rem;
-	}
-	@media (max-width: 600px) {
-		padding: 0;
-	}
+    padding: 0 0 0 10rem;
+    @media (max-width: 991px) {
+        padding: 0 0 0 4rem;
+    }
+    @media (max-width: 600px) {
+        padding: 0;
+    }
 `;
+
